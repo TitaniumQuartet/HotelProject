7038e1622da7f0f5c533b9ca03b28781e36016e9
LYReezy <greatlyr@163.com>
2016-10-15 09:15:55 +0800

    stub_driver

---
+++ HotelManagementSystem/stub_driver/src/main/java/tiquartet/stub_driver/App.java
@@ -0,0 +1,13 @@
+package tiquartet.stub_driver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
