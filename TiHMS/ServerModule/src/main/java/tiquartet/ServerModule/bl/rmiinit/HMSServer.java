bd813debbddac6d707033f18af183a0eb6d7438f
LYReezy <greatlyr@163.com>
2016-11-26 20:31:49 +0800

    Java1.8修改

--- TiHMS/ServerModule/src/main/java/tiquartet/ServerModule/bl/rmiinit/HMSServer.java
+++ TiHMS/ServerModule/src/main/java/tiquartet/ServerModule/bl/rmiinit/HMSServer.java
@@ -1,5 +1,25 @@
 package tiquartet.ServerModule.bl.rmiinit;
 
-public class HMSServer {
+import tiquartet.CommonModule.util.ResultMessage;
 
+/**
+ * 酒店管理系统的服务器类，可建立RMI服务，运行服务器.
+ * @author greatlyr
+ */
+public class HMSServer {
+	/**
+	 * 服务器初始化，提供RMI服务.
+	 * @return 启动服务器的结果信息
+	 */
+	public ResultMessage init() {
+		if (System.getSecurityManager() == null) {
+			System.setSecurityManager(new SecurityManager());
+		}
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return new ResultMessage(true);
+	}
 }
---
+++ TiHMS/ServerModule/src/main/java/tiquartet/ServerModule/bl/rmiinit/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 包含提供服务器RMI服务并启动服务器的类.
+ * @author greatlyr
+ * 
+ */
+package tiquartet.ServerModule.bl.rmiinit;
