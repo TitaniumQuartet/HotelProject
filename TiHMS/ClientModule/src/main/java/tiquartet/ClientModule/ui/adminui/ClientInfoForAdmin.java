66f12d8669c5465e29195bf52ccda5a87b746ad9
LYReezy <greatlyr@163.com>
2016-12-05 21:54:59 +0800

    没电了...

---
+++ TiHMS/ClientModule/src/main/java/tiquartet/ClientModule/ui/adminui/ClientInfoForAdmin.java
@@ -0,0 +1,70 @@
+package tiquartet.ClientModule.ui.adminui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import tiquartet.CommonModule.vo.UserVO;
+
+public class ClientInfoForAdmin {
+
+    @FXML
+    private Label field1;
+
+    @FXML
+    private Label data1;
+
+    @FXML
+    private Label field2;
+
+    @FXML
+    private Label data2;
+
+    @FXML
+    private Label field3;
+
+    @FXML
+    private Label data3;
+
+    @FXML
+    private Label field4;
+
+    @FXML
+    private Label field5;
+
+    @FXML
+    private Label data4;
+
+    @FXML
+    private Label data5;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField input2;
+
+    @FXML
+    private TextField input3;
+
+    @FXML
+    private TextField input4;
+
+    @FXML
+    private TextField input5;
+
+    @FXML
+    private Label titleLabel;
+
+    @FXML
+    void onSignUpClicked(ActionEvent event) {
+
+    }
+    
+    public void setData(UserVO clientInfo){
+    	
+    }
+
+}
+
