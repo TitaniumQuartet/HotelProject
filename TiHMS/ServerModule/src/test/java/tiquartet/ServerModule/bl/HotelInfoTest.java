9f12079b3a2c8e44a57fddd90f4856f21e1ebf5e
TrouBleMaKerV <782024062@qq.com>
2016-12-21 21:43:30 +0800

    修改

--- TiHMS/ServerModule/src/test/java/tiquartet/ServerModule/bl/HotelInfoTest.java
+++ TiHMS/ServerModule/src/test/java/tiquartet/ServerModule/bl/HotelInfoTest.java
@@ -4,6 +4,7 @@ package tiquartet.ServerModule.bl;
 import static org.junit.Assert.*;
 
 import java.rmi.RemoteException;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,52 +12,101 @@ import tiquartet.ServerModule.bl.hotelinfobl.HotelInfo;
 import tiquartet.ServerModule.bl.manageuserbl.ManageUserController;
 import tiquartet.CommonModule.util.ResultMessage;
 import tiquartet.CommonModule.vo.HotelBriefVO;
+import tiquartet.CommonModule.vo.HotelDetailsVO;
 import tiquartet.CommonModule.vo.HotelInfoVO;
 import tiquartet.CommonModule.vo.PreOrderVO;
 import tiquartet.CommonModule.vo.ReviewVO;
+import tiquartet.CommonModule.vo.RoomTypeVO;
 
 public class HotelInfoTest {
 	
 	private HotelInfo hotelinfo=new HotelInfo();	
-    private ManageUserController manageuser=new ManageUserController();
 	@Test
-	public void testgetHotelBrief() {
-         int districtID=12345;
-         String hotelName="nandaheyuan";
-         ResultMessage result=manageuser.addHotel(districtID, hotelName);
-         int userID=123456;
-         try {
-			HotelBriefVO hotelBrief=hotelinfo.getHotelBrief(Integer.parseInt(result.message), userID);
-			assertEquals(hotelBrief.hotelName,hotelName);
-		} catch (NumberFormatException | RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public void testgetHotelBrief() throws RemoteException {
+		 HotelBriefVO hotelBrief=hotelinfo.getHotelBrief(101001,1);
+         assertEquals(hotelBrief.hotelName,"NumOne");
+         assertEquals(hotelBrief.star,4);
+         assertEquals(hotelBrief.introduction,"best");
+         assertEquals(hotelBrief.cityName,"nj");
+         assertEquals(hotelBrief.circleName,"xl");
 	}
 	
 	@Test
-	public void testgetHotelDetails(){
-		
+	public void testgetHotelDetails() throws RemoteException{
+		HotelDetailsVO hoteldetails=hotelinfo.getHotelDetails(101001,1);
+		assertEquals(hoteldetails.address,"nju");
+		if(hoteldetails.averageg==4.4){
+			assertEquals(true,true);
+		}else{
+			assertEquals(true,false);
+		}
+		assertEquals(hoteldetails.circleName,"xl");
+		assertEquals(hoteldetails.cityName,"nj");
+		assertEquals(hoteldetails.hotelID,101001);
+		assertEquals(hoteldetails.introduction,"best");
+		if(hoteldetails.lowprice==100){
+			assertEquals(true,true);
+		}else{
+			assertEquals(true,false);
+		}
+		assertEquals(hoteldetails.serviceintro,"wifi");
+		assertEquals(hoteldetails.star,4);
 	}
 	
 	@Test
 	public void testavailableRoomType() throws RemoteException{
-		PreOrderVO preOrder=new PreOrderVO();
-		//List<RoomTypeVO> roomtype=hotel.availableRoomType(preOrder);
-		//assertEquals(roomtype.get(0).price,100);
-		//assertEquals(roomtype.get(1).roomType,"家庭房");
+		PreOrderVO preorder=new PreOrderVO();
+		preorder.userID = 1;
+		preorder.hotelID = 101001;
+		preorder.clientRealName="ccc";
+		preorder.hotelName = "NumOne";
+		preorder.startTime = "2017-01-05 12:00:00";
+		preorder.leaveTime = "2017-01-06 12:00:00";
+		preorder.roomType = 1;
+		preorder.roomTypeName = "";
+		preorder.userName = "Teki";
+		preorder.price = 300;
+		preorder.numOfRoom = 1;
+		List<RoomTypeVO> roomtype=hotelinfo.availableRoomType(preorder);
+		for(int i=0;i<roomtype.size();i++){
+			if(roomtype.get(i).roomTypeId==1){
+				assertEquals(roomtype.get(i).roomType,"home");
+				assertEquals(roomtype.get(i).typeIntroduction,"forhome");
+				if(roomtype.get(i).price==300){
+					assertEquals(true,true);
+				}else{
+					assertEquals(true,false);
+				}
+			}else if(roomtype.get(i).roomTypeId==2){
+				assertEquals(roomtype.get(i).roomType,"one");
+				assertEquals(roomtype.get(i).typeIntroduction,"forone");
+				if(roomtype.get(i).price==100){
+					assertEquals(true,true);
+				}else{
+					assertEquals(true,false);
+				}
+			}else if(roomtype.get(i).roomTypeId==3){
+				assertEquals(roomtype.get(i).roomType,"house");
+				assertEquals(roomtype.get(i).typeIntroduction,"great");
+				if(roomtype.get(i).price==500){
+					assertEquals(true,true);
+				}else{
+					assertEquals(true,false);
+				}
+			}
+		}
 		assertEquals(true, true);
 	}
 	
 	@Test
 	public void testreviewHotel() throws RemoteException{
 		ReviewVO review=new ReviewVO();
-		review.hotelID=12301456;
+		review.hotelID=101001;
 		review.review="非常好";
 		review.score=5;
-		review.userID=123456;
-		review.userName="qin";
-		review.time="2016-12-12 00:00:00";
+		review.userID=1;
+		review.userName="Teki";
+		review.time="2016-12-20 00:00:00";
 		ResultMessage result=hotelinfo.reviewHotel(review);
 		assertEquals(result.result,true);
 	}
@@ -64,16 +114,17 @@ public class HotelInfoTest {
 	@Test
 	public void testmodifyHotelInfo() throws RemoteException{
 		HotelInfoVO hotelInfo=new HotelInfoVO();
-		hotelInfo.address="nanjing";
-		hotelInfo.averageGrade=100;
-		hotelInfo.circleId=12301;
-		hotelInfo.cityName="nanjing";
+		hotelInfo.address="nju";
+		hotelInfo.averageGrade=5;
+		hotelInfo.circleId=201;
+		hotelInfo.cityName="nj";
 		hotelInfo.highprice=200;
-		hotelInfo.hotelID=12301456;
-		hotelInfo.hotelIntroduction="好酒店";
-		hotelInfo.hotelName="nanjing";
-		hotelInfo.lowprice=50;
-		hotelInfo.star=5;
+		hotelInfo.hotelID=201001;
+		hotelInfo.hotelIntroduction="verygood";
+		hotelInfo.serviceIntroduction="wifi";
+		hotelInfo.hotelName="Numthree";
+		hotelInfo.lowprice=100;
+		hotelInfo.star=4;
 		ResultMessage result=hotelinfo.modifyHotelInfo(hotelInfo);
 		assertEquals(result.result,true);
 	}
